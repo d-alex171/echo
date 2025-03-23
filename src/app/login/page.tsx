cd590014ecf5e515c3d70ed696717932063dac5e
Gary Hilares <dev.garyhilares@gmail.com>
2025-03-23 10:08:42 -0700

    Changed logo in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const LoginPage = () => {
   return (
     <div style={{ margin: "10vh auto", width: "50%", textAlign: "center" }}>
-      <Image src="/logo.png" width={400} height={200} alt="Echo logo" />
+      <Image src="/logo.png" width={600} height={300} alt="Echo logo" />
       <SpotifyAuth />
     </div>
   );
