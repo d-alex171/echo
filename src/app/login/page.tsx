5001fa9a45a13141d2ce431c519a7105fe18a763
Gary Hilares <dev.garyhilares@gmail.com>
2025-03-23 10:02:36 -0700

    Added login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,13 @@
 import SpotifyAuth from "../components/loginSpotifyButton";
+import Image from "next/image";
 
 const LoginPage = () => {
-    return (
-      <div>
-        <h1>Login with Spotify</h1>
-        <SpotifyAuth />
-      </div>
-    );
-  };
-  
-  export default LoginPage;
+  return (
+    <div style={{ margin: "10vh auto", width: "50%", textAlign: "center" }}>
+      <Image src="/logo.png" width={400} height={200} alt="Echo logo" />
+      <SpotifyAuth />
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import type { AppProps } from "next/app";
@@ -6,11 +6,10 @@ import { AuthProvider, useAuth } from "./context/AuthContext"; // Assuming your 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Header from "./components/header";
-import TracksPage from "./components/tracksShelf"
-import styles from './home.module.css'
+import TracksPage from "./components/tracksShelf";
+import styles from "./home.module.css";
 
-
-export default function App({ Component, pageProps }: AppProps){
+export default function App({ Component, pageProps }: AppProps) {
   const { isLoggedIn, profile } = useAuth();
   const router = useRouter();
 
@@ -26,14 +25,16 @@ export default function App({ Component, pageProps }: AppProps){
   }
 
   return (
-    <div className={styles['main']}>
-      <div className={styles['header']}>
-        <Header img_url={profile.images[0].url} name={profile?.display_name}/>
+    <div className={styles["main"]}>
+      <div className={styles["header"]}>
+        <Header
+          img_url={profile.images[0] && profile.images[0].url}
+          name={profile?.display_name}
+        />
       </div>
-      <div className={styles['songShelf']}>
+      <div className={styles["songShelf"]}>
         <TracksPage></TracksPage>
       </div>
-      
     </div>
   );
 }
